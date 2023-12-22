197304a6583aaccfc85178dd2fc7f96a98073c9f
NoahAkkad <noah.akkad@gmail.com>
2023-12-22 19:02:23 +0100

    Still Work

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebMarket.Data;
+using WebMarket.Models;
+
+namespace WebMarket.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<Category> opjCategoryList = _db.Categories.ToList();
+            return View();
+        }
+    }
+}
+
--- Migrations/20231222164622_SeedGategoryTable.Designer.cs
+++ Migrations/20231222164622_SeedGategoryTable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebMarket.Data;
 
@@ -10,9 +11,11 @@ using WebMarket.Data;
 namespace WebMarket.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231222164622_SeedGategoryTable")]
+    partial class SeedGategoryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

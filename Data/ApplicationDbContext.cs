056e405c814631f326fea6dc8850a1d52017c393
NoahAkkad <noah.akkad@gmail.com>
2023-12-22 15:54:43 +0100

    Sql Work

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebMarket.Models;
+
+namespace WebMarket.Data
+{
+	public class ApplicationDbContext: DbContext 
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<Category> Categories { get; set; }
+	}
+}
+
--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using WebMarket.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options=>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+
+
 
 var app = builder.Build();
 
